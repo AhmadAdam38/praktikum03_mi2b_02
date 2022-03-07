cff26b55b884c278082472387fdefb3f799287b7
AhmadAdam38 <ahmadadam0130@gmail.com>
2022-03-08 00:55:09 +0700

    aplikasi jadi

--- lib/main.dart
+++ lib/main.dart
@@ -24,16 +24,27 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
-  void ditekan(){
-    setState(() {
-      
-    });
-  }
-
-  Widget build(BuildContext context) {
     double _inputUser=0;
     double _kelvin=0;
+    double _reamur=0;
+
     final _adam=TextEditingController();
+
+    void _ditekan(){
+    setState(() {
+      _inputUser=double.parse(_adam.text);
+      _kelvin=_inputUser+273.15;
+      _reamur=(_inputUser*4)/5;
+    });
+    }
+    
+    void dispose(){
+      _adam.dispose();
+      super.dispose();
+    }
+
+  Widget build(BuildContext context) {
+
 
     return MaterialApp(
       title: 'Flutter Demo',
@@ -55,8 +66,9 @@ class _MyAppState extends State<MyApp> {
                 controller: _adam,
                 keyboardType: TextInputType.number,
                 inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                
                 decoration: InputDecoration(
-                  hintText: 'Masukkan Suhu Dalam Celcius'
+                  hintText: 'Masukkan Suhu Dalam Celcius',
                 ),
               ),
               Row(
@@ -70,7 +82,7 @@ class _MyAppState extends State<MyApp> {
                         child: Text("Suhu dalam Kelvin"),
                       ),
                       Text(
-                        "50",
+                        '$_kelvin',
                         style: TextStyle(
                           fontSize: 60,
                         ),
@@ -85,7 +97,7 @@ class _MyAppState extends State<MyApp> {
                         child: Text("Suhu dalam Reamor"),
                       ),
                       Text(
-                        "200",
+                        '$_reamur',
                         style: TextStyle(
                           fontSize: 60,
                         ),
@@ -100,7 +112,7 @@ class _MyAppState extends State<MyApp> {
                   onPrimary: Colors.blue,
                   onSurface: Colors.blue,
                 ),
-                onPressed: null,
+                onPressed: _ditekan,
                 child: Text(
                   "Konversi Suhu",
                   style: TextStyle(
@@ -117,87 +129,3 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
